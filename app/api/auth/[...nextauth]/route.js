926bc13a8b322cd370b8fcb5d9ca9205ff1bdee7
drasimov <wang_aaron_@hotmail.com>
2025-02-12 23:58:47 +0800

    Broken but a good save point

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,8 +7,33 @@ const authOptions = {
             clientId: process.env.AZURE_AD_CLIENT_ID,
             clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
             tenantId: process.env.AZURE_AD_TENANT_ID,
+            authorization: {
+                params: {
+                    redirect_uri: 'http://localhost:3000/api/auth/callback/azure-ad', 
+                    // response_type: 'code',
+                    scope: 'openid profile',
+                },
+            },
         }),   
-    ]
+    ],
+    callbacks: {
+        async jwt({ token, account, profile, code }) {
+            // Persist the OAuth access_token to the token right after signin
+            // console.log(account);
+            console.log(profile);
+            // console.log(code);
+            if (account) {
+              token.name = account.name
+            }
+            return token
+          },
+          async session({ session, token, user }) {
+            // Send properties to the client, like an access_token from a provider.
+            session.name = token.name
+            return session
+          }
+        }
+            
 };
 
 const handler = NextAuth(authOptions);
