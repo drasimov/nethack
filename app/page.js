b5ebfe793cbb051a0fe3658c19df346a7d349f59
drasimov <wang_aaron_@hotmail.com>
2025-02-17 15:57:08 +0800

    Main page change

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ const Home = () => {
       <p>Welcome to the <span className="serifBold big"><span className="cWhite">2nd Annual</span> <span className="cGreen">BIBS·C Network Hackathon</span></span>.</p>
       <hr/>
       <p className="cBlue">We are all about <span className="serifBold">understanding</span> and <span className="serifBold">applying</span> technology. In a Hackathon, you are given limited time to draw on your skills and produce an original product. Your product may be focused on anything from programming to hardware to art - so long as it answers the competition's prompts.</p>
-      <p className="cYellow">The 2025 Competition is starting! Prompts will be released very soon. In the meantime, try to <Link className="button bGray" href = "/login">Login</Link> to your Hackathon account!</p>
+      <p className="cYellow">The 2025 Competition has begun! If you have registered, please <Link className="button bGray" href = "/login">Login</Link> to your Hackathon account to view your project Dashboard and submit materials. Please note that this site experience as a beta test. </p>
       <center className="console">
         <Countdown key={0} targetDate={countdownDates[0]} label="Registration Closes"/>
         <Countdown key={1} targetDate={countdownDates[1]} label="Prompts Release"/>
