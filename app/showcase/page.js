22fccbdd7f1ea975525518ffc2f10d04e64a47af
drasimov <wang_aaron_@hotmail.com>
2025-04-23 02:27:51 +0800

    Added all pics

--- app/showcase/page.js
+++ app/showcase/page.js
@@ -9,7 +9,7 @@ import SubmissionPresent from '@/components/SubmissionPresent';
 
 const Dashboard = () => {
     const { data: session } = useSession();
-    const [entries, setEntries] = useState([]);
+    const [entries, setEntries] = useState("Loading...");
 
     const fetchEntries = async () => {
         if(session){
@@ -41,10 +41,14 @@ const Dashboard = () => {
     }, [session]);
 
     const iconAward = (
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-award" viewBox="0 0 16 16">
-            <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702z"/>
+        // <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-award" viewBox="0 0 16 16">
+        //     <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702z"/>
+        //     <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
+        // </svg>    
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-award-fill" viewBox="0 0 16 16">
+            <path d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864z"/>
             <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
-        </svg>    
+        </svg>
     )
     return (
         <>
@@ -53,6 +57,8 @@ const Dashboard = () => {
             <p className="cBlue">As a <span className="serifBold">Judge</span>, this is where you can survey and manage the competition submission.</p><br/>
             <p>The Hackathon submissions for 2025R1 can be found below. Some notes on judging:</p>
             <div>
+            {entries != "Loading..." && (
+                <>
                 <div className = "border gold">
                     <SubmissionPresent key = "0" submission ={entries[0]} override = {0}></SubmissionPresent>
                     <div className = "award">{iconAward}</div>
@@ -86,7 +92,8 @@ const Dashboard = () => {
                     <SubmissionPresent key = "7" submission ={entries[7]} override = {0}></SubmissionPresent>
                     <div className = "award">{iconAward}</div>
                 </div>
-
+                </>
+            )}
             </div>
         </>
     );
