04f6a80020fc828c20840eb72629d39a47d60beb
david-why <david_why@outlook.com>
2025-05-09 22:32:47 +0800

    Change navbar to say "Logout" when user is logged in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 "use client"
 import Link from "next/link";
 import styles from './Navbar.module.css'; 
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
+  const { status: authStatus } = useSession();
   return (
     <div className={styles.nav}>
         <div className={styles.wrap}>
             <Link href = "/">BIBS·C Network Hackathon</Link>
-            <Link href = "/login" className = {`${styles.right} med`}>Login</Link>
+            <Link href = "/login" className = {`${styles.right} med`}>{authStatus === 'authenticated' ? 'Logout' : 'Login'}</Link>
         </div>
     </div>
   );
